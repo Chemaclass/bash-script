7cad9dbb60977cf5be86d7ee7110ce7756de6888
José María Valera Reales <chemaclass@outlook.es>
2014-05-04 23:36:24 +0200

    Elimino 'Encontrar más apps en el centro de soft'

--- basico.sh
+++ basico.sh
@@ -26,20 +26,6 @@ sudo apt-get install indicator-synapse
 # Instalar Temas, Iconos etc…
 sudo apt-get install elementary-blue-theme elementary-champagne-theme elementary-colors-theme elementary-dark-theme elementary-harvey-theme elementary-lion-theme elementary-milk-theme elementary-plastico-theme elementary-whit-e-theme elementary-elfaenza-icons elementary-emod-icons elementary-enumix-utouch-icons elementary-nitrux-icons elementary-taprevival-icons elementary-thirdparty-icons elementary-plank-themes elementary-wallpaper-collection
 
-#######################################################
-# Encontrar más aplicaciones en el Centro de Software #
-#######################################################
-# OpenShot, editor de video
-# Thunderbird, e-mail
-# Firefox, explorador web (No te recomiendo Chromium ni Google Chrome)
-# Pidgin, chat
-# Transmission, torrents
-# VLC, video
-# XBMC, media center
-# FileZilla, FTP
-# GIMP, editor de imágenes (tipo Photoshop)
-# LibreOffice, Suite de ofimática (MS Office pero libre)
-
 #########################################
 # OpenJdk Java: 						#
 # 	sudo apt-get install openjdk-7-jdk  #
