c33b22141a1c27ac8afdd0babda9db28030a9824
José María Valera Reales <chemaclass@outlook.es>
2014-05-05 21:35:46 +0200

    Script para recuperar el audio del laptop

---
+++ recuperar_audio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##################################
+# PulseAudio. 
+#	Servidor de sonido multiplataforma
+##################################
+echo "Rerconfigurando el sonido."
+sudo killall pulseaudio 
+rm -r ~/.config/pulse/*
+rm -r ~/.pulse*
+echo "Espera unos segundos..."
+sleep 3
+pulseaudio -k 
+echo "¡Ya tienes listo el audio!"
