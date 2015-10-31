ad99d7f713bf8cc3f82eb4b494f530c34344b319
Chemaclass <chemaclass@outlook.es>
2015-10-31 16:07:59 +0100

    Add user_verified as optional bool

--- lovoo_vs_chema.js
+++ lovoo_vs_chema.js
@@ -7,6 +7,7 @@
 
 // .dev
 var from_where = "berlin";
+var only_verified = true;
 var each_ms = 200;
 var with_log = true;
 var Btn = function(n){
@@ -23,14 +24,15 @@ var Btn = function(n){
 var btn_yes = new Btn(1), btn_no = new Btn(0);
 var si = setInterval(function() {
     var u = $("div[ng-if='user'] div:nth-child(2) .h6");
+    var is_u_verified = (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1);
+    var is_u_from_where = (u.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where) != 0);
     var name = u.prev().text();
-    if ((u.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where) != 0)
-        && (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1)) {
+    if (is_u_from_where && (!only_verified || (only_verified && (is_u_verified)))) {
         btn_yes.click(name);
     } else {
         btn_no.click(name);
     }
-    if (with_log) console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);
+    if (with_log) console.log("y: " + btn_yes.names.length + ", n: " + btn_no.names.length);
 }, each_ms);
 
 
