b04efdc45d89b81612ff8e73cc66905c78dd2ba7
Chemaclass <chemaclass@outlook.es>
2015-10-31 16:01:39 +0100

    Improve the .min

--- lovoo_vs_chema.js
+++ lovoo_vs_chema.js
@@ -6,6 +6,9 @@
 */
 
 // .dev
+var from_where = "berlin";
+var each_ms = 200;
+var with_log = true;
 var Btn = function(n){
     var btn = $("span[ng-click='voteUser("+n+"); $event.stopPropagation();']");
     this.names = [];
@@ -17,11 +20,7 @@ var Btn = function(n){
         'names':this.names
     }
 };
-var btn_yes = new Btn(1);
-var btn_no = new Btn(0);
-var from_where = "berlin";
-var each_ms = 200;
-var with_log = true;
+var btn_yes = new Btn(1), btn_no = new Btn(0);
 var si = setInterval(function() {
     var u = $("div[ng-if='user'] div:nth-child(2) .h6");
     var name = u.prev().text();
@@ -36,6 +35,11 @@ var si = setInterval(function() {
 
 
 // .min
-var Btn = function(n){btn = $("span[ng-click='voteUser("+n+"); $event.stopPropagation();']");this.names = [];return {'click':function(name) {btn.click();this.names.push(name);},'names':this.names}}, btn_yes = new Btn(1), btn_no = new Btn(0), si=setInterval(function(){u=$("div[ng-if='user'] div:nth-child(2) .h6"),name=u.prev().text();((u.find("div:nth-child(1)").text().toLowerCase().indexOf("berlin") != 0) && (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1))? btn_yes.click(name) : btn_no.click(name); console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);}, 200);
+var from_where="berlin",each_ms=200,with_log=true,
+Btn = function(n){ this.names = [];return {'click':function(name){$("span[ng-click='voteUser("+n+"); $event.stopPropagation();']").click();this.names.push(name);},'names':this.names}},
+btn_yes = new Btn(1), btn_no = new Btn(0),
+si=setInterval(function(){u=$("div[ng-if='user'] div:nth-child(2) .h6"),name=u.prev().text();
+((u.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where) != 0) && (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1))
+?btn_yes.click(name) : btn_no.click(name); if(with_log)console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);}, each_ms);
 
 // clearInterval(si)
