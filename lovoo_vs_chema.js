233e467bd9c5e8594d6452b87e310129c19762eb
Chemaclass <chemaclass@outlook.es>
2015-10-31 16:40:29 +0100

    Improve .min

--- lovoo_vs_chema.js
+++ lovoo_vs_chema.js
@@ -12,12 +12,13 @@
 */
 
 // .dev
+// ======================== BEGIN ========================
 var from_where = "berlin";
 var only_verified = true;
 var each_ms = 200;
 var with_log = true;
-var Btn = function(n){
-    var btn = $("span[ng-click='voteUser("+n+"); $event.stopPropagation();']");
+var Btn = function(n) {
+    var btn = $("span[ng-click='voteUser(" + n + "); $event.stopPropagation();']");
     this.names = [];
     return {
         'click':function(name) {
@@ -40,14 +41,16 @@ var si = setInterval(function() {
     }
     if (with_log) console.log("y: " + btn_yes.names.length + ", n: " + btn_no.names.length);
 }, each_ms);
+// ========================== END ==========================
 
+// (.min) The same but minified
+var from_where="berlin",only_verified=!0,each_ms=200,with_log=!0,
+Btn=function(n){var e=$("span[ng-click='voteUser("+n+"); $event.stopPropagation();']");return this.names=[],{click:function(n){e.click(),this.names.push(n)},names:this.names}},
+btn_yes=new Btn(1),btn_no=new Btn(0),
+si=setInterval(function(){var n=$("div[ng-if='user'] div:nth-child(2) .h6"),
+e=-1==n.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no "),
+t=0!=n.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where),i=n.prev().text();
+t&&(!only_verified||only_verified&&e)?btn_yes.click(i):btn_no.click(i),
+with_log&&console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length)},each_ms);
 
-// .min
-var from_where="berlin",each_ms=200,with_log=true,
-Btn = function(n){ this.names = [];return {'click':function(name){$("span[ng-click='voteUser("+n+"); $event.stopPropagation();']").click();this.names.push(name);},'names':this.names}},
-btn_yes = new Btn(1), btn_no = new Btn(0),
-si=setInterval(function(){u=$("div[ng-if='user'] div:nth-child(2) .h6"),name=u.prev().text();
-((u.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where) != 0) && (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1))
-?btn_yes.click(name) : btn_no.click(name); if(with_log)console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);}, each_ms);
-
-// clearInterval(si)
+// Remember -> You can stop the execution with: 'clearInterval(si)'
