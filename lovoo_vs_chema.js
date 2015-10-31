e09e1a220a5de25b756a8aa9e5bc5e1cbd92628d
Chemaclass <chemaclass@outlook.es>
2015-10-31 15:54:22 +0100

    create from_where,each_ms&with_log var

--- lovoo_vs_chema.js
+++ lovoo_vs_chema.js
@@ -1,6 +1,8 @@
 /**
-*
 * @author: José María Valera Reales
+*
+* I like Lovoo app, but sometimes can be funnier, especially when you can play and
+* doing another things at the same time, you know :-)
 */
 
 // .dev
@@ -17,17 +19,20 @@ var Btn = function(n){
 };
 var btn_yes = new Btn(1);
 var btn_no = new Btn(0);
+var from_where = "berlin";
+var each_ms = 200;
+var with_log = true;
 var si = setInterval(function() {
     var u = $("div[ng-if='user'] div:nth-child(2) .h6");
     var name = u.prev().text();
-    if ((u.find("div:nth-child(1)").text().toLowerCase().indexOf("berlin") != 0)
+    if ((u.find("div:nth-child(1)").text().toLowerCase().indexOf(from_where) != 0)
         && (u.find("div:nth-child(3) div").text().toLowerCase().indexOf(" no ") == -1)) {
         btn_yes.click(name);
     } else {
         btn_no.click(name);
     }
-    console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);
-}, 200);
+    if (with_log) console.log("y: "+btn_yes.names.length+", n: "+btn_no.names.length);
+}, each_ms);
 
 
 // .min
