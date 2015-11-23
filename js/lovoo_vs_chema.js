e31bb0ece1d585c16c83e7ff6a0d4e75140394c6
Chemaclass <chemaclass@outlook.es>
2015-11-24 00:42:15 +0100

    js: Add some docu

--- js/lovoo_vs_chema.js
+++ js/lovoo_vs_chema.js
@@ -13,14 +13,35 @@
 
 // .dev
 // ======================== BEGIN ========================
+
+/** @var string City to searh. */
 var from_where = "berlin";
+/** @var bool Care about if the person was verified before. */
 var only_verified = true;
+/** @var int Time to exec the function to check the person. */
 var each_ms = 200;
+/** @var bool Show the logger via console.log. */
 var with_log = true;
-var Person = function(name,age) {
+/** @var int Limit of persons to check. -1 means infinite loop. */
+var limit = -1;
+
+/**
+* Person object.
+* 
+* With the members [name, age, city, verifed]
+*/
+var Person = function(name, age, city, verified) {
     this.name = name;
     this.age = age;
+    this.city = city;
+    this.verified = verified;
 };
+
+/**
+* Button object. 
+*
+* Basically here we can manage easily the list of persons and the onlick event. 
+*/
 var Btn = function(n) {
     var btn = $("span[ng-click='voteUser(" + n + "); $event.stopPropagation();']");
     this.persons = [];
@@ -32,26 +53,52 @@ var Btn = function(n) {
         'persons':this.persons
     }
 };
-var btn_yes = new Btn(1), btn_no = new Btn(0);
+
+/** @var Btn btn_yes */
+var btn_yes = new Btn(1);
+
+/** @var Btn btn_no */
+var btn_no = new Btn(0);
+
+/**
+* Do each time. This is the main function.
+* 
+* Check the each person and click to "yes" or "no" if the 
+* person is someone who probably we are interested.
+*/
 var si = setInterval(function() {
     var u = $("div[ng-if='user'] div:nth-child(2) .h6");
     var is_u_verified = (u.find("div:nth-child(3) div")
         .text().toLowerCase().indexOf(" no ") == -1);
-    var is_u_from_where = (u.find("div:nth-child(1)")
-        .text().toLowerCase().indexOf(from_where.toLowerCase()) != 0);
+    var city = u.find("div:nth-child(1)").first().text().toLowerCase().split(" ")[1];
+    var is_u_from_where = (city.indexOf(from_where.toLowerCase()) != 0);
     var name_age = u.prev().text().split(", ");
     var name = name_age[0];
     var age = name_age[1];
+    
+    // Create the person.
+    var person = new Person(name, age, city, is_u_verified);
+    // Put the person inside the correct Btn object.
     if (is_u_from_where && (!only_verified || (only_verified && (is_u_verified)))) {
-        btn_yes.click(new Person(name, age, city, verified));
+        btn_yes.click(person);
     } else {
-        btn_no.click(new Person(name, age, city, verified));
+        btn_no.click(person);
     }
+    
+    // Should we print the log?
+    var persons_length = (btn_yes.persons.length + btn_no.persons.length);
     if (with_log) {
-        console.log("yes: " + btn_yes.persons.length + ", no: " + btn_no.persons.length
-            + ", total: " + (btn_yes.persons.length+btn_no.persons.length));
+        console.log("yes: " + btn_yes.persons.length 
+            + ", no: " + btn_no.persons.length
+            + ", total: " + persons_length);
+    }
+    
+    // Check if we should to stop it.
+    if (limit != -1 && persons_length >= limit) {
+        clearInterval(si);
     }
 }, each_ms);
+
 // ========================== END ==========================
 
 // (.min) The same but minified
