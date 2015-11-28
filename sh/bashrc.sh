960d06afb3f8214add6dab6737bc82b8876be1d0
Chemaclass <chemaclass@outlook.es>
2015-11-28 15:50:59 +0100

    Load all sh/functions via bashrc file

--- sh/bashrc.sh
+++ sh/bashrc.sh
@@ -2,6 +2,20 @@
 # Terminal config #
 ###################
 
+echo "Load all sh/functions as aliases ["
+# Load all sh functions
+for f in $(find ~/scripts-for-fun/sh/functions/ -type f -name "*.sh")
+do
+	source $f
+	b=$(basename $f)
+    extension="${b##*.}"
+    filename="${b%.*}"
+	alias $filename=function_$filename
+	echo "  $filename => $b, "
+done
+echo "]"
+
+# Print the cool PS1. With the current git branch
 function parse_git_branch {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "("${ref#refs/heads}")"
