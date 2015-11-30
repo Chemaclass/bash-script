001d15c72bce396585b6df843ac3f1f1920d9dc7
Chemaclass <chemaclass@outlook.es>
2015-11-30 23:28:58 +0100

    Rename searh.sh to find2.sh file

--- sh/functions/find2.sh
+++ sh/functions/find2.sh
@@ -1,5 +1,5 @@
 #
-# Search is another way to use find.
+# findd is another way to use find.
 #
 # @param string $1 Name
 # @param string $2 Path
@@ -9,7 +9,7 @@
 # @param int $4 exec to each file found it.
 #		Deafult: false
 #
-function_search() {
+function_find2() {
     name=${1}
     path=${2-.}    
     type=${3-f}
@@ -21,7 +21,7 @@ function_search() {
 	fi
 
 	if [[ "false" == "$exec" ]]; then
-        find $path -type $type -name $name	
+        find $path -type $type -name $name
 	else
         find $path -type $type -name $name -exec $exec 
 	fi
