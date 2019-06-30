3632ae3ea82e60aa8c86f7151070dcac7a8acb48
Chemaclass <chemaclass@outlook.es>
2019-06-30 19:19:37 +0200

    Add concept and task definition for random_fb_picker script

---
+++ js/random_fb_picker.js
@@ -0,0 +1,13 @@
+/**
+ * Concept
+ * ============
+ * I need to select some users randomly from a given list, where these users
+ * have to have done some actions. For example, given a certain Facebook post,
+ * they have to: shared it and/or liked it and/or commented on it.
+ */
+
+/**
+ * Task:
+ * Print all user names that shared a given Facebook post.
+ * AC: Run it directly using the browser console.
+ */
