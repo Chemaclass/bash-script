13f9b5488d88ddaa263d9cc07db0368b3d2cc8dc
Chemaclass <chemaclass@outlook.es>
2019-06-30 20:02:48 +0200

    implemented MVP for printUserNames

--- js/random_fb_picker.js
+++ js/random_fb_picker.js
@@ -11,3 +11,53 @@
  * Print all user names that shared a given Facebook post.
  * AC: Run it directly using the browser console.
  */
+function printUserNames() {
+  "use strict";
+  class User {
+    constructor(name) {
+      this.name = name;
+      this.didShared = false; // TODO: specify whether it was shared|liked|commented
+    }
+  }
+
+  class UserList {
+    constructor() {
+      this.users = [];
+    }
+    add(user) {
+      if (!this._exists(user)) {
+        this.users.push(user);
+      }
+    }
+    _exists(user) {
+      let exists = false;
+      this.users.forEach(currentUser => {
+        if (currentUser.name === user.name) {
+          exists = true;
+        }
+      });
+      return exists;
+    }
+    all() {
+      return this.users;
+    }
+  }
+
+  const users = new UserList();
+
+  const sharedPostSelector =
+    "#repost_view_dialog > div div.clearfix a.profileLink";
+  document.querySelectorAll(sharedPostSelector).forEach(function(el) {
+    const userName = el.getAttribute("title");
+    if (userName !== null) {
+      users.add(new User(userName));
+    }
+  });
+
+  users.all().forEach((user, i) => {
+    const { name } = user;
+    console.log(`id${i}, name:${name}`);
+  });
+}
+
+printUserNames();
