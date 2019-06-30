65c4e5d99d7e49889f6806d981eef1f4dc5d1e8a
Chemaclass <chemaclass@outlook.es>
2019-06-30 21:06:18 +0200

    Create findRandomUsers

--- js/random_fb_picker.js
+++ js/random_fb_picker.js
@@ -19,15 +19,15 @@
   }
 
   class UserList {
-    constructor() {
-      this.users = [];
+    constructor(users) {
+      this.users = users || [];
     }
     add(user) {
-      if (!this._exists(user)) {
+      if (!this.exists(user)) {
         this.users.push(user);
       }
     }
-    _exists(user) {
+    exists(user) {
       let exists = false;
       this.users.forEach(currentUser => {
         if (currentUser.name === user.name) {
@@ -39,9 +39,15 @@
     all() {
       return this.users;
     }
+    total() {
+      return this.users.length;
+    }
+    getByIndex(i) {
+      return this.users[i];
+    }
   }
 
-  function crawlUserInfo(selector, nameAttribute) {
+  function crawlUserList(selector, nameAttribute) {
     const users = new UserList();
     document.querySelectorAll(selector).forEach(function(el) {
       const userName = el.getAttribute(nameAttribute);
@@ -53,14 +59,37 @@
   }
 
   function printUserNames(users) {
-    users.all().forEach((user, i) => {
+    users.all().forEach((user, index) => {
       const { name } = user;
-      console.log(`id${i}, name:${name}`);
+      console.log(`id${index}, name:${name}`);
     });
+  }
+
+  function findRandomUsers(users, total, maxTries = 1000) {
+    const randomUsers = new UserList();
+    let tries = 0;
+    while (randomUsers.total() < total) {
+      if (tries > maxTries) {
+        console.log("Emergency exit due to a reached limit!");
+        break;
+      }
+      let randomUser = users.getByIndex(
+        Math.floor(Math.random() * users.total())
+      );
+      if (randomUser && !randomUsers.exists(randomUser)) {
+        randomUsers.add(randomUser);
+      }
+      tries++;
+    }
+    return randomUsers;
   }
 
   const sharedSelector = "#repost_view_dialog > div div.clearfix a.profileLink";
   const nameAttribute = "title";
-  const usersThatSharedThePost = crawlUserInfo(sharedSelector, nameAttribute);
+  const usersThatSharedThePost = crawlUserList(sharedSelector, nameAttribute);
+
+  console.log("Total users");
   printUserNames(usersThatSharedThePost);
+  console.log(`Random users`);
+  printUserNames(findRandomUsers(usersThatSharedThePost, 5));
 })();
